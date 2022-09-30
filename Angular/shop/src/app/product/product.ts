275acfb444c70a1f4b069f76dcb9f82e8914b6b9
ÜMİT AKTAŞ <umitaktass@hotmail.com>
2022-09-30 19:31:55 +0300

    Front-end and Angular
    Angular project I coded while learning frontend.

---
+++ Angular/shop/src/app/product/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+
+    id: number = 0;
+    name: string = " ";
+    price: number = 0;
+    categoryId: number = 0;
+    description: string = " ";
+    imageUrl: string = " ";
+
+}
